6d2b7bc2f2f797fa6233aad3863e69a0754ccf26
Md. Shakil <shakil.eub.cse@gmail.com>
2025-02-24 02:31:49 +0600

    template master

--- inventory_management/inventory_management_app/views.py
+++ inventory_management/inventory_management_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context={
+        "title": "Dashboard",
+        "subTitle": "AI",
+    }
+    return render(request,"index.html", context)
